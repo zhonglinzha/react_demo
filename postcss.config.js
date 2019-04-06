6bd446206874734d81419c3a74f2562aa2cd8639
zhonglin.zha <774640601@qq.com>
2019-04-06 11:26:24 +0800

    up

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    plugins: [
+      require('autoprefixer')({
+        browsers: [
+            'Android 2.3',
+            'Android >= 4',
+            'Chrome >= 35',
+            'Firefox >= 31',
+            'Explorer >= 8',
+            'iOS >= 7',
+            'Opera >= 12',
+            'Safari >= 7.1'
+        ]
+      })
+    ]
+  }
---
+++ webpack/webpack.base.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebPackPlugin = require('html-webpack-plugin');//模版配置
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');//抽离css文件
+
+module.exports = {
+
+
+
+
+
+}
